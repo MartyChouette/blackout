30c770a8117828f4470badbfe27f7d21ec8823f8
Marty <martychouette@gmail.com>
2025-04-10 12:46:16 -0400

    added blackout screen toggletoggle black scren with the 'O' key

--- Assets/blackout_on_off.cs
+++ Assets/blackout_on_off.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
-public class blackout_on_off : MonoBehaviour
+public class ToggleCanvas : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Assign your Canvas GameObject from the Inspector.
+    // Make sure this Canvas is not the same GameObject as the one this script is attached to.
+    public GameObject targetCanvas;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Listen for the 'O' key press.
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            // Ensure targetCanvas is assigned.
+            if (targetCanvas != null)
+            {
+                // Toggle the active state:
+                // If active, deactivate; if inactive, activate.
+                targetCanvas.SetActive(!targetCanvas.activeSelf);
+            }
+            else
+            {
+                Debug.LogWarning("targetCanvas is not assigned in the Inspector.");
+            }
+        }
     }
 }
